52b82efa850e0931643ed305969e2eb12777c3f7
Daniel McGrath <djmcgrath95@gmail.com>
2023-10-10 14:13:23 -0600

    completed plantshow mock cc

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewPlantForm() {
+function NewPlantForm({handleNewPlant}) {
+  const [plantForm, setPlantForm] = useState({
+    name: "",
+    image: "",
+    price: 0
+  })
+
+  function handleChange (e) {
+    setPlantForm({
+      ...plantForm,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleSubmit (e) {
+    e.preventDefault()
+    const plantData = {
+      name: plantForm.name,
+      image: plantForm.image,
+      price: plantForm.price
+    }
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(plantData)
+    })
+    .then((res) => res.json())
+    .then((newPlant) => handleNewPlant(newPlant))
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          name="name" 
+          placeholder="Plant name"
+          onChange={handleChange}
+        />
+        <input 
+          type="text" 
+          name="image" 
+          placeholder="Image URL" 
+          onChange={handleChange}
+        />
+        <input 
+          type="number" 
+          name="price" 
+          step="0.01" 
+          placeholder="Price" 
+          onChange={handleChange}
+        />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
+
 function PlantPage() {
+  const [plantInfo, setPlantInfo] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => {
+        setPlantInfo(data)
+      })
+  }, [])
+
+  function handleNewPlant (newPlant) {
+    setPlantInfo([...plantInfo], newPlant)
+  }
+
+  const filteredPlants = plantInfo.filter((plant) => {
+    return plant.name.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm handleNewPlant={handleNewPlant}/>
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plantInfo={filteredPlants} />
     </main>
   );
 }
